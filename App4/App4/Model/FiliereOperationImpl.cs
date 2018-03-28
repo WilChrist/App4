ad11b9103d1c5dc42401f020dc5a447e65d21679
imane5 <imaneherraz16@gmail.com>
2018-03-28 13:02:53 +0100

    second commit

---
+++ App4/App4/Model/FiliereOperationImpl.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace App4.Model
+{
+    class FiliereOperationImpl : IFiliereOperation
+    {
+        SQLite.SQLiteConnection database;
+        public FiliereOperationImpl(SQLite.SQLiteConnection database)
+        {
+            this.database = database;
+        }
+        public int CreateFiliere(Filiere filiere)
+        {
+            return database.Insert(filiere);
+        }
+
+        public void DeleteFiliere(Filiere filiere)
+        {
+            database.Delete(filiere);
+        }
+
+        public List<Filiere> ReadFilieres()
+        {
+            return database.Table<Filiere>().ToList();
+        }
+
+        public void UpdateFiliere(Filiere filiere)
+        {
+            database.Update(filiere);
+        }
+    }
+}
